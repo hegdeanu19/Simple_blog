fc66542f47ea7b8059e1dcf6b826603ae9cf4b3a
LENOVO <hegdenagashree19@gmail.com>
2022-02-05 21:15:51 -0800

    commit

--- theblog/views.py
+++ theblog/views.py
@@ -1,5 +1,18 @@
+from pdb import post_mortem
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+
+from .models import Post
+
+
+
+
 
 # Create your views here.
-def home(request):
-    return render(request,'theblog/home.html',{})
+#def home(request):
+    #return render(request,'theblog/home.html',{})
+
+class HomeView(ListView):
+    model=Post
+    template_name='theblog/home.html'
+    
